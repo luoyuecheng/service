9373665c2de8dd88d1ba66f60ab7888b8f20cf7c
luoyuecheng <luoyuecheng.me@gmail.com>
2019-08-25 22:24:49 +0800

    models

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
 var app = express();
 
 const routes = require('./routes');
@@ -8,24 +11,41 @@ for (let key in routes) {
   app.use(routes[key]);
 }
 
-// listence port
-var port = 3001;
+// const url = 'mongodb://182.61.138.124:27017';
+// const dbName = 'meetion';
+// let server = null;
 
+// MongoClient.connect(url, function (err, client) {
+//   assert.equal(null, err);
+//   assert.ok(client != null);
+
+//   const db = client.db(dbName);
+//   const collection = db.collection('example');
+
+//   server = listen(4200);
+// });
+
+let port = 3001;
+const server = listen(port);
+
+// listen port, start up server
 function listen (port, callback) {
-  return app.listen(port, function (...args) {
+  let server = app.listen(port, function (...args) {
     if (callback && typeof callback === 'function') {
       return callback.call(this, port);
     }
     const address = server && server.address() || {};
     console.log('app address `%s` http://%s:%s', address.family, address.address, address.port)
   })
+
+  // listen server, when the port is occupied, the port number is incremented
+  server.on('error', function (e) {
+    if (e.code === 'EADDRINUSE') {
+      console.log('Error: ', e, '\nport', port);
+      port += 1;
+      server.close();
+      server = listen(port);
+    }
+  })
+  return server;
 }
-var server = listen(port);
-server.on('error', function (e) {
-  if (e.code === 'EADDRINUSE') {
-    console.log('Error: ', e, '\nport', port);
-    port += 1;
-    server.close();
-    server = listen(port);
-  }
-})
--- routes/demo.js
+++ routes/demo.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const db = require('../models/db');
 
 const router = express.Router();
 
 router.get('/api', function (req, res) {
+})
+
+router.get('/api/example', function (req, res) {
+    db(req, res);
 })
 
 module.exports = router;

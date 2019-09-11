b82ac0df4ea1dce8dd8c10303d49451cf02b3545
luoyuecheng <luoyuecheng.me@gmail.com>
2019-09-11 15:25:18 +0800

    router '/'

--- routes/webapp.js
+++ routes/webapp.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.use('/demo', express.static('web/demo'));
 
+router.use('/', express.static('web/blog'));
 router.use('/blog', express.static('web/blog'));
 
 module.exports = router;
